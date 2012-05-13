d435469d3201d95e0691e71aa21b736ed7fea25b
enr <enrico.comiti@gmail.com>
2012-05-13 18:23:00 +0200

    cleanup pluginExcludes

--- JsonRestApiGrailsPlugin.groovy
+++ JsonRestApiGrailsPlugin.groovy
@@ -12,13 +12,7 @@ class JsonRestApiGrailsPlugin {
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp",
-            "grails-app/domain/org/grails/plugins/rest/SimpleDomain.groovy",
-            "grails-app/domain/org/grails/plugins/rest/EdgeCaseDomain.groovy",
-            "grails-app/domain/org/grails/plugins/rest/WithEagerFieldsDomain.groovy",
-            "grails-app/domain/org/grails/plugins/rest/WithoutEagerFieldsDomain.groovy",
-            "grails-app/domain/org/grails/plugins/rest/NeverSaved.groovy",
-            "grails-app/domain/org/grails/plugins/rest/Address.groovy"
+            "grails-app/domain/**"
     ]
 
     def author = "Matthias Hryniszak"
